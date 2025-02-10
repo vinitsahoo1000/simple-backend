5be0f97c02e075b7dcba704ac85ddbe6fae8feb8
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-02-10 21:16:30 +0530

    multiply divide api added

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -15,3 +15,30 @@ userRouter.get("/sum",async(req:Request,res:Response):Promise<any>=>{
         msg: `sum of a and b is ${a + b}`
     })
 })
+
+userRouter.get("/divide",async(req:Request,res:Response):Promise<any>=>{
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        return res.status(400).send({ msg: "Invalid query parameters" });
+    }
+    
+    res.send({
+        msg: `sum of a and b is ${a / b}`
+    })
+})
+
+
+userRouter.get("/multiply",async(req:Request,res:Response):Promise<any>=>{
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        return res.status(400).send({ msg: "Invalid query parameters" });
+    }
+    
+    res.send({
+        msg: `sum of a and b is ${a * b}`
+    })
+})
